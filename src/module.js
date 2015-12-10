cc56ee49ebcd68d608032bc9bd90a82c9e13e130
Alex LaFroscia <alex@lafroscia.com>
2015-12-10 02:17:55 -0500

    Use browser history instead of hash
    Note: Allows the SSR to _actually_ work!

--- src/module.js
+++ src/module.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Router } from 'react-router';
 import { render } from 'react-dom';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
 import routes from 'routes';
 
+const history = createBrowserHistory();
+
 render(
-  <Router routes={routes} />,
+  <Router history={history} routes={routes} />,
   document.getElementById('content')
 );
