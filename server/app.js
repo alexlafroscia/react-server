06d9f52202780312871e48217029c5580d1d4dc7
Alex LaFroscia <alex@lafroscia.com>
2015-12-10 00:08:51 -0500

    Use react-router for server rendering

--- server/app.js
+++ server/app.js
@@ -1,15 +1,27 @@
 import express from 'express';
+import expHbs from 'express-handlebars';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { match, RoutingContext } from 'react-router';
 import routes from '../src/routes';
 
 const app = express();
+app.set('views', __dirname);
+app.engine('.hbs', expHbs({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
 
 app.use(function(req, res ) {
   match({ routes, location: req.url }, (error, redirectLocation, props) => {
-    const router = renderToString(<RoutingContext {...props} />);
-    res.status(200).send(router);
+    if (error) {
+      res.status(500).send(error.message);
+    } else if (redirectLocation) {
+      res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+    } else if (props) {
+      const router = renderToString(<RoutingContext {...props} />);
+      res.render('index', { router });
+    } else {
+      res.status(404).send('Not Found');
+    }
   });
 });
 
